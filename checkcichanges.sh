1dc5643d6038e0934d5973e27c57f5a236fa4b1b
Joshua <jschmidtnj@gmail.com>
2019-09-13 22:37:39 -0400

    added correct email templates

--- checkcichanges.sh
+++ checkcichanges.sh
@@ -4,12 +4,12 @@
 set -e
 
 changes() {
-  git diff --stat --cached -- api/
+  git diff --stat --cached -- api/ frontend/
 }
 
 travis_ignore="[skip ci]"
 
-if ! changes | grep -E "api/" ; then
+if ! changes | grep -E "api/|frontend/" ; then
   echo "no ci changes found"
   sed -i.bak -e "1s/^/$travis_ignore /" ".git/COMMIT_EDITMSG"
 else
