eb9f78a7b45b2aaa71482f8feef8f48bca672fc0
Joshua <jschmidtnj@gmail.com>
2020-02-15 14:12:37 -0500

    ci/cd: docs build
    fixed docs deployment
    
    Signed-off-by: Joshua <jschmidtnj@gmail.com>

--- docs/builddocs.sh
+++ docs/builddocs.sh
@@ -15,14 +15,14 @@ mv apidocs .vuepress/public/api
 # build
 yarn generate
 yarn pwa
-sed -i -e 's/\/images\//\/emailhacks\/images\//g' docs/Polyfills/manifest.json
+sed -i -e 's/\/images\//\/emailhacks\/images\//g' docs/manifest.json
 
-rm -rf docs/Polyfills/manifest.json-e
+rm -rf docs/manifest.json-e
 
 rm -rf .vuepress/dist/
 mv emailhacks/ .vuepress/dist
-rm -rf docs/Polyfills/web/Images/emailhacks
-cp -r docs/Polyfills/web/Images/ .vuepress/dist/images
-rm -rf docs/Polyfills/web/Images/
-mv docs/Polyfills/manifest.json .vuepress/dist/manifest.json
+rm -rf docs/web/Images/emailhacks
+cp -r docs/web/Images/ .vuepress/dist/images
+rm -rf docs/web/Images/
+mv docs/manifest.json .vuepress/dist/manifest.json
 rm -rf docs/
